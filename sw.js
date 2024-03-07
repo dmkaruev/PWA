956785c56694cab1153d9bb8cc1e1208a91682d7
dmkaruev <dmkaruev@mail.ru>
2024-03-07 19:55:52 +0300

    +

---
+++ sw.js
@@ -0,0 +1,32 @@
+const staticCacheName = 'site-static-v1';
+const assets = [
+    'images/logo.png',
+    'images/social.png',
+    'css/styles.css',
+    'js/script.js'
+];
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
+});
+self.addEventListener('activate', evt => {
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+self.addEventListener('fetch', evt => {
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
+});
